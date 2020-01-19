187ce5e4fd7c2d9841b6e6aae89fdbdd9a4d396b
warrioryoko <miangpellegri@gmail.com>
2020-01-19 13:37:32 -0800

    Finished menu display, and button loads sample fishes into state.

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
 
 class App extends React.Component {
   state = {
@@ -12,21 +14,27 @@ class App extends React.Component {
     // 1. Take a copy of the existing state
     const fishes = { ...this.state.fishes };
     // 2. Add new fish to fishes variable
-    fishes[`fish${Date.now()}`] = fish;
+    fishes[`fish${Date.now()}`] = fish; // makes a unique fish id, unless you submit faster than 1 per millisecond
     // 3. Set new fishes object to state
     this.setState({
       fishes // fishes: fishes
     });
     console.log("Adding a fish object...");
   };
+  loadSampleFishes = () => {
+    this.setState({fishes: sampleFishes});
+  }
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market"/>
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]} />)}
+          </ul>
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
       </div>
     );
   }
