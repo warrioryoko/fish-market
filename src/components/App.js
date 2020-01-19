67823f09a7d2fabc78506b774d4f242382094405
warrioryoko <miangpellegri@gmail.com>
2020-01-18 17:18:17 -0800

    Added form to buy fish. Added handler prop to App.

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,21 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+  addFish = fish => {
+    // 1. Take a copy of the existing state
+    const fishes = { ...this.state.fishes };
+    // 2. Add new fish to fishes variable
+    fishes[`fish${Date.now()}`] = fish;
+    // 3. Set new fishes object to state
+    this.setState({
+      fishes // fishes: fishes
+    });
+    console.log("Adding a fish object...");
+  };
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +26,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market"/>
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     );
   }
