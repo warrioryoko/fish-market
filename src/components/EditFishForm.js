ca7a90a300c6e70b6d4b9a4cdd9a1507ef552de8
warrioryoko <miangpellegri@gmail.com>
2020-01-25 16:29:03 -0800

    Added methods for removing inventory and order items via props.

--- src/components/EditFishForm.js
+++ src/components/EditFishForm.js
@@ -4,7 +4,7 @@ class EditFishForm extends React.Component {
   handleChange = event => {
     console.log(event.currentTarget.value);
     // update fish
-    // 1. Take a copy of the current fish, overwrite the thing that gets changed
+    // Take a copy of the current fish, overwrite the thing that gets changed
     const updatedFish = { 
       ...this.props.fish,
       [event.currentTarget.name]: event.currentTarget.value
@@ -18,29 +18,37 @@ class EditFishForm extends React.Component {
         type="text" 
         name="name" 
         onChange={this.handleChange} 
-        value={this.props.fish.name} />
+        value={this.props.fish.name}
+      />
       <input 
         type="text" 
         name="price" 
         onChange={this.handleChange}
-        value={this.props.fish.price} />
+        value={this.props.fish.price}
+      />
       <select 
         type="text" 
         name="status" 
         onChange={this.handleChange}
-        value={this.props.fish.status}>
+        value={this.props.fish.status}
+      >
           <option value="available">Fresh!</option>
           <option value="unavailable">Sold Out!</option>
       </select>
       <textarea 
         name="desc" 
         onChange={this.handleChange}
-        value={this.props.fish.desc} />
+        value={this.props.fish.desc}
+      />
       <input 
         type="text" 
         name="image" 
         onChange={this.handleChange}
-        value={this.props.fish.image} />
+        value={this.props.fish.image}
+      />
+      <button onClick={() => this.props.deleteFish(this.props.index)}>
+        Remove Fish
+      </button>
     </div>
   }
 }
